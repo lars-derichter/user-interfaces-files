6c7dd73d861b9340d4e78bda63ad8f263ed97274
Lars De Richter <lars.derichter@gmail.com>
2020-10-26 08:54:13 +0100

    Make the php echo the right fields

--- module-6/post-demo.php
+++ module-6/post-demo.php
@@ -7,11 +7,16 @@
 </head>
 <body>
   <h1>Je post data</h1>
-  <p>Normaal sla je die op in een databank of verstuur je ze in een e-mail, maar als demo bek echo.</p>
+  <p>Normaal sla je die op in een databank of verstuur je ze in een e-mail, maar als demo ben ik gewoon een echo.</p>
   <pre>
 <?php
-echo 'Value 1: ' . htmlspecialchars($_POST["value1"]) . "\n" .
-'Value 2: ' . htmlspecialchars($_POST["value2"]);
+echo
+'Voornaam: ' . htmlspecialchars($_POST["Voornaam"]) . "\n" .
+'Familienaam: ' . htmlspecialchars($_POST["Familienaam"]) . "\n" .
+'E-mail: ' . htmlspecialchars($_POST["E-mail"]) . "\n" .
+'Type vraag: ' . htmlspecialchars($_POST["Type-vraag"]) . "\n" .
+'Vraag: ' . htmlspecialchars($_POST["Vraag"]) . "\n" .
+'Nieuwbrief: ' . htmlspecialchars($_POST["Nieuwsbrief"]);
 ?>
   </pre>
 </body>
